bea0f096cab9a20b82d187bef48de704909b0dc3
Aryan <av868600@gmail.com>
2024-06-21 20:50:24 +0530

    check if the guess is right

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -21,5 +21,28 @@ else :
 random_num=random.randint(0,range)
 
 
+#checking if the user has guessed the correct number or not
+while True:
+    user_guess=input("Guess the number!!")
 
+    #checking if the user has entered the number or not
+    if user_guess.isdigit():
+        user_guess=int(user_guess)
+
+    else:
+        print("Only numeric values accepted")
+        continue
+
+    #checking if the user has guessed or not
+    if user_guess==random_num:
+        print("You got it!!!")
+        break
+
+    elif user_guess<random_num:
+        print("Enter a greater number.")
+
+    else:
+        print("Enter a smaller number")
+    
+print(random_num)
 
