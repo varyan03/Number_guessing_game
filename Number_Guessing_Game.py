fe9d2423798e8d79194f03e1404a2b8a847351ab
Aryan <av868600@gmail.com>
2024-06-21 20:44:34 +0530

    added range var

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,21 @@
+import random
+
+#asking the user to enter a number that is greater than 0
+range=input("Enter a number")
+
+#checking if its a digit and not 
+if range.isdigit():
+    range=int(range)
+
+    #checking if the number is less than 0 or not
+    if range<=0:
+        print("Enter a number greater than 0")
+        quit()
+
+    
+else :
+    print("Only Numerical values accepted ")
+    quit()
+
+
+
